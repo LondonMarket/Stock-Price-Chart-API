d2e80c8ae60a0981c3c65e4dc3fe70bbc4c36feb
Charlie <30829462+Charlie-Hill@users.noreply.github.com>
2022-10-09 18:58:47 +0100

    Add new Stock Data route to API

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express'
 import { PriceChartRoutes } from './priceChartRoutes';
+import { StockDataRoutes } from './stockData';
 
 const router: Router = Router();
 
 router.use('/pricechart', PriceChartRoutes);
+router.use('/stockdata', StockDataRoutes);
 
 export const MainRouter: Router = router;
