01efb3954c939243f9fdee90130bbe920ccbaf02
Charlie <30829462+Charlie-Hill@users.noreply.github.com>
2022-10-09 19:28:49 +0100

    Add basic canvas generation & API logic to test integration

--- api/src/routes/priceChartRoutes.ts
+++ api/src/routes/priceChartRoutes.ts
@@ -1,9 +1,17 @@
 import { NextFunction, Request, Response, Router } from 'express';
+import { CreateChart } from 'price-chart-core';
 
 const router: Router = Router();
 
 router.get('/', (req: Request, res: Response, next: NextFunction) => {
-    return res.status(200).json('Price Chart')
+
+    const image = CreateChart();
+
+    res.writeHead(200, {
+        "Content-Type": "image/jpg"
+    });
+
+    res.end(image);
 });
 
 export const PriceChartRoutes: Router = router;
---
+++ price-chart-core/index.ts
@@ -0,0 +1,24 @@
+import { createCanvas } from "canvas";
+
+
+export function CreateChart () {
+
+    const width = 1980;
+    const height = 1080;
+
+    const canvas = createCanvas(width, height);
+
+    const context = canvas.getContext('2d');
+
+    context.fillStyle = '#000';
+    context.fillRect(0, 0, width, height);
+
+    const text = 'Test Text';
+    context.font = 'bold 70pt Menlo';
+    context.textAlign = 'center';
+    context.fillStyle = '#fff';
+    context.fillText(text, 600, 170);
+
+    return canvas.toBuffer();
+}
+
